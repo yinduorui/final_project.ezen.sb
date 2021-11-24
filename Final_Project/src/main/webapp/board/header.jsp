2eaaa73bb25eb1f8e5bba98091d8f2d046cc1c3f
Daye Yoon <poiuylk56@gmail.com>
2021-11-24 11:28:57 +0900

    2021-11-24

--- Final_Project/src/main/webapp/board/header.jsp
+++ Final_Project/src/main/webapp/board/header.jsp
@@ -10,6 +10,11 @@
 <style>
 .img-thumbnail {border: 1px solid #fff;}
 .bg-light {background-color: #fff !important;}
+.btn-area2 button {width: 100px; height: 40px;
+	background: #ffc107;
+	border: none;
+	border-radius: 0.25rem;
+}
 </style>
 <title>Insert title here</title>
 </head>
@@ -18,13 +23,14 @@
 <nav class="navbar navbar-light bg-light">
   <div class="container">
   <img src="../img/logo.png" class="img-thumbnail" alt="logo">
-    <form class="d-flex">
+    <form class="d-flex" style="width:400px">
       <input class="form-control me-4" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-      
-      	<div class="main_btn">
+     </form>
+      	<div class="btn-area2">
 		<button type="submit" value="로그인" onclick="location.href=">로그인</button>
+		</div>
+		<div class="btn-area2">
 		<button type="submit" value="회원가입" onclick="location.href=">레시피 작성</button>
 		</div>
   </div>
--- Final_Project/src/main/webapp/js/main_swiper.js
+++ Final_Project/src/main/webapp/js/main_swiper.js
@@ -1,15 +1,50 @@
-      var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+var swiper = new Swiper(".mySwiper", {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	slidesPerGroup: 3,
+	loop: true,
+	loopFillGroupWithBlank: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
+/*
+$(document).ready(function() {
+	$('#CarrouselBox2 .slickList2').slick({
+		dots: false,
+		centerMode: false,
+		vertical: false,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		prevArrow: $('#CarrouselBox2 .btn_prev_exhibit_2'),
+		nextArrow: $('#CarrouselBox2 .btn_next_exhibit_2')
+	});
+
+	$('#cateList .cate_cont').slick({
+		dots: false,
+		centerMode: false,
+		vertical: false,
+		slidesToShow: 12,
+		slidesToScroll: 12,
+		prevArrow: $('#cateList .slick_event_prev'),
+		nextArrow: $('#cateList .slick_event_next')
+	});
+
+	$('#chefsList .chefList').slick({
+		dots: false,
+		centerMode: false,
+		vertical: false,
+		slidesToShow: 10,
+		slidesToScroll: 10,
+		prevArrow: $('#chefsList .slick_event_prev'),
+		nextArrow: $('#chefsList .slick_event_next')
+	});
+
+	$("#loading").hide();
+});
+*/
